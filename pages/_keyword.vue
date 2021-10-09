39780739e69de91f621f5327d7a53217e8081152
Ho Hang Au Yeung <mayeung@telus.net>
2021-10-08 17:27:25 -0700

    rename pages

--- pages/_keyword.vue
+++ pages/_keyword.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-card width="600px" elevation="0" class="mx-auto">
-      <v-card-text :elevation="0" >
+      <v-card-text :elevation="0">
         <v-text-field
           v-model="keyword"
           hide-details
@@ -12,7 +12,7 @@
           background-color="#FAFAFA"
         />
 
-        <SearchResults :keyword="keyword"  />
+        <SearchResults :keyword="keyword" />
       </v-card-text>
     </v-card>
   </v-app>
@@ -32,12 +32,15 @@ export default {
       mdiMagnify,
     }
   },
+  beforeMount() {
+    this.keyword = this.$route.params.keyword
+  },
 }
 </script>
 
 <style>
 html {
-  overflow: hidden
+  overflow: hidden;
 }
 </style>
 
